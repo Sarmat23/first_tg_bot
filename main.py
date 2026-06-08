e31de1f17e4d991e743ac11d4f3f4ea6bf02fc39
Sarmat23 <79203632217@yandex.ru>
2026-06-08 09:26:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ BOT_TOKEN = "8045514027:AAGhkexJ5AjQcIm95qDA1TQLIkYSd_vS-4s"
 
 CHANNEL_ID = "@darom_tkv"          # username или -100xxxxxxxxxx @darom_tkv
 MODERATOR_ID = 310342334               # Telegram ID модератора (число)
-GEMINI_API_KEY = "AQ.Ab8RN6Kn3R2Aw3cbScA1R_2JdFAcekZ-yhyj7-IbVU7PAXRJtw"
+GEMINI_API_KEY = "AIzaSyAZpG9ioi1Iw-27p1--h3U-sD4_XBQ10rQ"
 DB_PATH = "darom.db"
 AD_LIFETIME_DAYS = 30                  # через сколько дней зачёркивать объявление
 
